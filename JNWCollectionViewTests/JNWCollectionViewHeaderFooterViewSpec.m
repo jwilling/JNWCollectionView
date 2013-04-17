46baf0625ebb618ecc9d767b2893278d0965879c
CodaFi <devteam.codafi@gmail.com>
2013-04-16 22:14:22 -0600

    Rudimentary tests for JNWCollectionViewHeaderFooterView

---
+++ JNWCollectionViewTests/JNWCollectionViewHeaderFooterViewSpec.m
@@ -0,0 +1,13 @@
+//
+//  JNWCollectionViewHeaderFooterViewSpec.m
+//  JNWCollectionView
+//
+//  Created by Robert Widmann on 4/16/13.
+//  Copyright (c) 2013 AppJon. All rights reserved.
+//
+
+#import "JNWCollectionViewHeaderFooterViewSpec.h"
+
+@implementation JNWCollectionViewHeaderFooterViewSpec
+
+@end

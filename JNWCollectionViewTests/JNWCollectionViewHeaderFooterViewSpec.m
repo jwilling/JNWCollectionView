7677e4647e186430bfac24cd6e927d1a965e804d
CodaFi <devteam.codafi@gmail.com>
2013-04-16 22:29:58 -0600

    Re++Test

--- JNWCollectionViewTests/JNWCollectionViewHeaderFooterViewSpec.m
+++ JNWCollectionViewTests/JNWCollectionViewHeaderFooterViewSpec.m
@@ -6,8 +6,14 @@
 //  Copyright (c) 2013 AppJon. All rights reserved.
 //
 
-#import "JNWCollectionViewHeaderFooterViewSpec.h"
+SpecBegin(JNWCollectionViewHeaderFooterView)
 
-@implementation JNWCollectionViewHeaderFooterViewSpec
+describe(@"-reuseIdentifier", ^{
+	JNWCollectionViewHeaderFooterView *headerFooterView = [[JNWCollectionViewHeaderFooterView alloc]initWithReuseIdentifier:@"Identifier"];
+	
+	it(@"Should return the same reuse identifier it was initialized with", ^{
+		expect(headerFooterView.reuseIdentifier).to.equal(@"Identifier");
+	});
+});
 
-@end
+SpecEnd

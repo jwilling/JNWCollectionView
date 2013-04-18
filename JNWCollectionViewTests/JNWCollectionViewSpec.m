d25e714f7fa725198d2c44db691d9140e2a7a5ca
Jonathan Willing <jwilling@me.com>
2013-04-17 21:20:16 -0500

    add test for basic selection

--- JNWCollectionViewTests/JNWCollectionViewSpec.m
+++ JNWCollectionViewTests/JNWCollectionViewSpec.m
@@ -41,4 +41,20 @@ describe(@"data source", ^{
 	});
 });
 
+describe(@"selection", ^{
+	__block JNWCollectionView *collectionView = nil;
+	
+	beforeAll(^{
+		collectionView = [[JNWCollectionView alloc] initWithFrame:CGRectMake(0, 0, 300, 500)];
+	});
+	
+	it(@"should select and return the same selection", ^{
+		NSIndexPath *expected = [NSIndexPath jnw_indexPathForItem:1 inSection:0];
+		NSIndexPath *wrong = [NSIndexPath jnw_indexPathForItem:0 inSection:0];
+		[collectionView selectItemAtIndexPath:expected atScrollPosition:JNWCollectionViewScrollPositionNone animated:NO];
+		expect([collectionView indexPathsForSelectedItems][0]).to.equal(expected);
+		expect([collectionView indexPathsForSelectedItems][0]).notTo.equal(wrong);
+	});
+});
+
 SpecEnd

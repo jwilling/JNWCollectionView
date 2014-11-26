1c80e4d2cff5d84b4d6bc7c062a1bf6af0faab30
Ilija Tovilo <ilija.tovilo@me.com>
2014-11-26 22:00:48 +0100

    Reset demo back to original

--- demo/JNWCollectionViewDemo/ListDemoViewController.m
+++ demo/JNWCollectionViewDemo/ListDemoViewController.m
@@ -30,7 +30,6 @@ static NSString * const headerIdentifier = @"HEADER";
 	self.collectionView = [[JNWCollectionView alloc] initWithFrame:self.view.bounds];
 	self.collectionView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
 	self.collectionView.dataSource = self;
-    self.collectionView.allowsEmptySelection = NO;
 	[self.view addSubview:self.collectionView];
 	
 	JNWCollectionViewListLayout *layout = [[JNWCollectionViewListLayout alloc] init];

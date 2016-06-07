6175f302966dd564e045de0ff1fd169e284e56c8
Frédéric Leboucher <mjr.kettch@gmail.com>
2016-06-07 11:42:59 +0200

    Fixing indentation

--- JNWCollectionView/JNWCollectionViewCell.m
+++ JNWCollectionView/JNWCollectionViewCell.m
@@ -187,8 +187,8 @@
 }
 
 - (void)rightMouseDown:(NSEvent *)theEvent {
-  [super rightMouseDown:theEvent];
-  
+	[super rightMouseDown:theEvent];
+
 	[self.collectionView rightClickInCollectionViewCell:self withEvent:theEvent];
 }
 
